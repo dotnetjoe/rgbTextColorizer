726c366adc0b57510e124f6c7c2b4b1cd65473bb
ronaldogdm <ronaldogarciadem@gmail.com>
2019-07-24 16:54:26 -0400

    Initial Commit

---
+++ rgbTextColorizer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rgbTextColorizer
+//
+//  Created by Adam Alston on 7/24/19.
+//  Copyright © 2019 Adam Alston. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
